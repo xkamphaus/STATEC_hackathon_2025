056b1ddce072f227c081cd8251977c20c41fa844
Daniel MICHULKE <MicDa147@men.lux>
2025-10-28 11:27:38 +0100

    add markers for hospitals and pharmacies

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const map = L.map('map').setView([49.6, 6.1], 10);
 
 // Predefined markers for each option
 const markers = {
-    option1: [
-        L.marker([49.61, 6.13]).bindPopup('Marker 1-1'),
-        L.marker([49.62, 6.15]).bindPopup('Marker 1-2')
+    hospitals: [
+        //L.marker([49.61, 6.13]).bindPopup('Marker 1-1'),
+		L.marker([49.618701, 6.101091]).bindPopup('Centre Hospitalier de Luxembourg (CHL)'),
+		L.marker([49.853445, 6.09465]).bindPopup('Centre Hospitalier du Nord (CHdN)'),
+		L.marker([49.632902, 6.176345]).bindPopup('Hôpital Kirchberg (HRS)'),
+		L.marker([49.501616, 5.98207]).bindPopup('Centre Hospitalier Émile Mayrisch (CHEM)'),
+		L.marker([49.633, 6.159]).bindPopup('Clinique Bohler (Hôpitaux Robert Schuman Group)'),
+		L.marker([49.60389, 6.1289]).bindPopup('ZithaKlinik (Hôpitaux Robert Schuman Group)')
+
     ],
-    option2: [
-        L.marker([49.59, 6.11]).bindPopup('Marker 2-1'),
-        L.marker([49.60, 6.09]).bindPopup('Marker 2-2')
+    pharmacies: [
+        L.marker([49.59, 6.11]).bindPopup('Pharmacy 2-1'),
+        L.marker([49.60, 6.09]).bindPopup('Pharmacy 2-2')
     ],
     option3: [
         L.marker([49.58, 6.12]).bindPopup('Marker 3-1'),
@@ -17,11 +23,36 @@ const markers = {
     ]
 };
 
+function updateMarkerIcons(markerGroup, icon) {
+    markerGroup.forEach(marker => {
+        marker.setIcon(icon);
+    });
+}
+const hospitalIcon = L.icon({
+    iconUrl: 'icons/hospital.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 20],
+    popupAnchor: [1, -34],
+    shadowSize: [50, 50]
+});
+const pharmacyIcon = L.icon({
+    iconUrl: 'icons/pharmacy.svg',
+    iconSize: [20, 20],
+    iconAnchor: [10, 10],
+    popupAnchor: [1, -34],
+    shadowSize: [50, 50]
+});
+updateMarkerIcons(markers.hospitals, hospitalIcon);
+updateMarkerIcons(markers.pharmacies, pharmacyIcon);
+
+
 // Add OpenStreetMap tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 19
 }).addTo(map);
+
+// age_ratio
 
 // Style function for GeoJSON features
 function style(feature) {
@@ -73,7 +104,7 @@ Object.values(markers).flat().forEach(marker => marker.addTo(map));
 
 // Checkbox event listeners
 document.getElementById('mark_hospitals').addEventListener('change', function(e) {
-    markers.option1.forEach(marker => {
+    markers.hospitals.forEach(marker => {
         if (e.target.checked) {
             marker.addTo(map);
         } else {
@@ -83,7 +114,7 @@ document.getElementById('mark_hospitals').addEventListener('change', function(e)
 });
 
 document.getElementById('mark_pharmacies').addEventListener('change', function(e) {
-    markers.option2.forEach(marker => {
+    markers.pharmacies.forEach(marker => {
         if (e.target.checked) {
             marker.addTo(map);
         } else {
